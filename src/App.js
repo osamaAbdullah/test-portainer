1f071f7143202883c7ea1c2823c4d7a58016f10e
osama_akre <osama@akre.com>
2022-09-19 18:41:29 +0300

    some changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Osama hate react
+          Osama hate react but love vue
         </p>
       </header>
     </div>
