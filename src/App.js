0e4967299c3fb24c5dd7d2c09814c37600383ff0
osama_akre <osama@akre.com>
2022-09-21 12:49:49 +0300

    some changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Ahmed hate react but love vue and only Vue
+          Hama hate react but love vue and only Vue
         </p>
       </header>
     </div>
